8e6de1f38aefd3a6b976df2cbc67256e442104f9
Marcelin <lenforiee@gmail.com>
2025-08-27 20:12:12 +0200

    aktualizacja plików configuracyjnych

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+module.exports = {
+    images: {
+        formats: ['image/avif', 'image/webp'],
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'assets.vercel.com',
+                port: '',
+                pathname: '/image/upload/**',
+            },
+            {
+                protocol: 'https',
+                hostname: '**.fbcdn.net',
+            },
+        ],
+    },
 };
-
-export default nextConfig;
